a32b49916496d8cbbb0a66b2dce36e8512eb91dc
Xavier <xaviramos668@gmail.com>
2023-03-09 23:45:08 -0300

    Finalizado

--- js/formMobile.js
+++ js/formMobile.js
@@ -3,15 +3,43 @@ var boton2=document.querySelector(".derecha")
 var panel=document.querySelector(".opciones")
 var panelDer=document.querySelector(".panelDer")
 var panelIzq=document.querySelector(".panelIzq")
-boton1.addEventListener("click",function()
+var mediaQuery = window.matchMedia('(max-width: 1000px)');
+function mover() 
 {
-    panel.style.left="50%"
-    panelDer.style.display="flex"
-    panelIzq.style.display="none"
-})
-boton2.addEventListener("click",function()
-{
-    panel.style.left="0%"
-    panelDer.style.display="none"
-    panelIzq.style.display="flex"
-})
+    if (mediaQuery.matches) 
+    {
+        boton1.addEventListener("click",function()
+        {
+            panel.style.left="0%"
+            panel.style.top="50%"
+            panelDer.style.display="flex"
+            panelIzq.style.display="none"
+        })
+        boton2.addEventListener("click",function()
+        {
+            panel.style.left="0%"
+            panel.style.top="0%"
+            panelDer.style.display="none"
+            panelIzq.style.display="flex"
+        })
+    } 
+    else 
+    {
+        boton1.addEventListener("click",function()
+        {
+            panel.style.top="0%"
+            panel.style.left="50%"
+            panelDer.style.display="flex"
+            panelIzq.style.display="none"
+        })
+        boton2.addEventListener("click",function()
+        {
+            panel.style.top="0%"
+            panel.style.left="0%"
+            panelDer.style.display="none"
+            panelIzq.style.display="flex"
+        })
+    }
+}
+mover()
+mediaQuery.addListener(mover);
