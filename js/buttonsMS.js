19eaf076ff5e318616d616fd961a9ae14dec1d24
Xavier <xaviramos668@gmail.com>
2023-03-10 16:19:09 -0300

    Taller

--- js/buttonsMS.js
+++ js/buttonsMS.js
@@ -1,14 +1,30 @@
-var botones=document.querySelectorAll(".menu_item");
-var botonesSlider=document.querySelectorAll(".slider_item")
-var secciones=document.querySelectorAll(".seccion")
-for(let i=0;i<botones.length;i++)
-{
-    botones[i].addEventListener("click",function(event)
-    {
+var botones = document.querySelectorAll(".menu_item");
+var botones2 = document.querySelectorAll(".menu_item2")
+var botonesSlider = document.querySelectorAll(".slider_item")
+var botonesSlider2 = document.querySelectorAll(".slider_item2")
+var secciones = document.querySelectorAll(".seccion")
+var buttonT = document.querySelector(".btaller")
+const sect = ["inicio", "nosotros", "talleres", "contacto"]
+for (let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("click", function (event) {
         secciones[i].scrollIntoView({ behavior: 'smooth' });
     })
-    botonesSlider[i].addEventListener("click",function(event)
-    {
+    botonesSlider[i].addEventListener("click", function (event) {
         secciones[i].scrollIntoView({ behavior: 'smooth' });
     })
 }
+for (let i = 0; i < botones2.length; i++) {
+
+    botones2[i].addEventListener("click", function (event) {
+        window.location.href = "index.html#" + sect[i];
+    })
+    botonesSlider2[i].addEventListener("click", function (event) {
+        window.location.href = "index.html#" + sect[i];
+    })
+}
+if (buttonT != null)
+    (
+        buttonT.addEventListener("click", function () {
+            window.location.href = "talleres.html";
+        })
+    )
